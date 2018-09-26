2b2f84fbfe6be9eb9d4e0ce904282ad756e42668
Maayan Kfir <mayankfir@gmail.com>
2018-09-26 21:21:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,11 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_value == nil || smallest_value > value
       smallest_value = value
-      smallest_key = key 
+      smallest_key = key
     end
   end
   key
-      
+
     end
 
 end
